7ac8e4e9094f629ab93bcaa27c0c5d21785b73de
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-01-19 18:02:40 -0300

    Parametros variaveis

---
+++ funcao/paramsVariaveis.js
@@ -0,0 +1,13 @@
+function soma() {
+  let soma = 0
+  for (i in arguments) {
+    soma += arguments[i]
+  }
+  return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1,5,2,4,5))
+console.log(soma(1.2,4,6,3, "teste"))
+console.log(soma('Teste', "a", "V"))
